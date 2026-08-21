83ad2e7abd0f2fd33968e5b5277b21fa6c181956
midari <noemiazevdo@gmail.com>
2026-08-21 20:41:42 -0300

    javas

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
-document.getElementById('projeto').addEventListener('click' , () => console.log ('clicado')
-)
+let btn_teste = document.getElementById("teste")
 
-setTimeout(() => console.log("oi") , 5000)
+btn_teste.addEventListener("click", () => {
+    btn_teste.innerHTML = "outro"
+    btn_teste.style.backgroundColor = "black"
 
+})
+
+let jumbo = document.querySelector('#jumbo')
+
+btn_teste.addEventListener("click", () => {
+    jumbo.src = "img/gatinho.jpg"
+    // ou jumbo.setAttribute("src", "img/gatinho.jpg")
+})
+
+let header = document.querySelector('header')
+let controle = false
+btn_teste.addEventListener("click", () => {
+    if (controle == false) {
+        header.style.display = "none"
+        controle = true
+    }
+    else {
+        header.style.display = "flex"
+        controle = false
+    }
+    header.style.display = "none"
+})
+
+//let cards = document.getElementsByClassName('card')
+//cards.forEach(element => {
+//element.addEventListener ("mouseover"), ()=> 
+//element.backgroundColor = "black"
+//});
+
+let cards = document.getElementsByClassName('card')
+
+for (const item of cards) {
+    item.addEventListener("mouseover", () => {
+        item.style.backgroundColor = "black"
+    })
+    item.addEventListener("mouseout", () => {
+        item.style.backgroundColor = "#0fa3b1"
+    })
+}
