2f7006715b5a09780e0f8730a2795995abe85b4f
noemi <noemiazevedo@gmail.com>
2026-09-16 19:34:11 -0300

    cont projeto

--- views/categoria/livro/detalhes.php
+++ views/categoria/livro/detalhes.php
@@ -5,17 +5,25 @@ $id = $ GET['id'];
 $livro - Livro::buscarPorId($id);
 
 ?>
-<main class="main-detalhe" id="main2">
+<main class="main-detalhe" >
     <div id="img-detalhe">
-        <img src="https://picsum.photos/300/400" alt="">
+        <?php if($livro['capa']==null):?>
+            <img src="https://picsum.photos/300/400" alt="">
+        <?php else: ?>
+            <img src="/projeto/img/gatinho.jpg/<? =$livro['capa']?>" alt="">
+        <?php endif; ?>
 
     </div>
     <div id="texto-detalhe">
-        <h2>titulo</h2>
+        <h2><?= $livro['titulo'] ?></h2>
         <br>
-        <p>2026</p>
+        <p><?= $livro['resumo'] ?></p>
         <br>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea aspernatur cumque odio culpa aperiam, omnis rerum cum at eaque fugit. Tenetur eligendi, praesentium a nemo voluptatibus quibusdam aspernatur quisquam hic!</p>
+        <p><?= $livro['ano_pub'] ?></p>
+        <br>
+        <p><?= $livro['autor'] ?></p>
+        <p>Categoria</p>
 
     </div>
 
